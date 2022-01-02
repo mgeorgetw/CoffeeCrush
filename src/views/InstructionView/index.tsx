f33cf66dbee111b164c5a8e49bc8021423f15734
Zhaoyuan Huang <mgeorgetw@gmail.com>
2022-01-03 01:38:27 +0800

    Code cleanup.

--- src/views/InstructionView/index.tsx
+++ src/views/InstructionView/index.tsx
@@ -1,11 +1,25 @@
 import { useState, useEffect } from "react";
 import { TypeBrewMethod } from "../../types/TypeBrewMethod";
+import styles from "./InstructionView.module.css";
 import { Navigation } from "./Navigation";
+import { Stopwatch } from "./Stopwatch";
 import { ProgressDonutChartButton } from "./ProgressDonutChartButton";
 import { StepsList } from "./StepList";
-import { Stopwatch } from "./Stopwatch";
-import styles from "./InstructionView.module.css";
 import { roundToInteger } from "../../utils/math";
+
+export const FractionIndicator = ({
+  fraction,
+  total,
+  unit,
+}: {
+  fraction: number;
+  total: number;
+  unit: string;
+}) => (
+  <div className={styles.largeDigit}>{`${roundToInteger(
+    fraction
+  )}/${roundToInteger(total)}${unit}`}</div>
+);
 
 export const InstructionView = ({
   methodDetails,
@@ -18,12 +32,12 @@ export const InstructionView = ({
   isReady: boolean;
   setIsReady: Function;
 }) => {
+  const [isRunning, setIsRunning] = useState<boolean>(false);
   const [time, setTime] = useState<number>(0);
   const [currentStep, setCurrentStep] = useState<number>(1);
   const [untilNextStep, setUntilNextStep] = useState<number>(
     methodDetails.steps[0].duration
   );
-  const [isRunning, setIsRunning] = useState<boolean>(false);
   const [addedWater, setAddedWater] = useState<number>(0);
 
   const calculatedSteps = methodDetails.steps.map((step) => ({
@@ -34,6 +48,9 @@ export const InstructionView = ({
       ($0: string, $1: string) => roundToInteger(+$1 * water)
     ),
   }));
+
+  const waterAfterCurrentStep =
+    calculatedSteps[currentStep - 1].fractionOfWater;
 
   const stepsLeft = calculatedSteps.length - currentStep;
 
@@ -46,22 +63,6 @@ export const InstructionView = ({
     setAddedWater(0);
   };
 
-  const waterForCurrentStep = calculatedSteps[currentStep - 1].fractionOfWater;
-
-  const FractionIndicator = ({
-    fraction,
-    total,
-    unit,
-  }: {
-    fraction: number;
-    total: number;
-    unit: string;
-  }) => (
-    <div className={styles.largeDigit}>{`${roundToInteger(
-      fraction
-    )}/${roundToInteger(total)}${unit}`}</div>
-  );
-
   useEffect(() => {
     if (!isRunning) return;
 
@@ -72,7 +73,7 @@ export const InstructionView = ({
       if (stepsLeft > 0 && untilNextStep === 1) {
         setCurrentStep((prevStep) => prevStep + 1);
         setUntilNextStep(calculatedSteps[currentStep].duration);
-        waterForCurrentStep && setAddedWater(waterForCurrentStep);
+        waterAfterCurrentStep && setAddedWater(waterAfterCurrentStep);
       }
     }, 1000);
 
@@ -83,7 +84,7 @@ export const InstructionView = ({
     currentStep,
     calculatedSteps,
     stepsLeft,
-    waterForCurrentStep,
+    waterAfterCurrentStep,
   ]);
   return (
     <div className={`${styles.slider} ${!isReady && styles.close}`}>
