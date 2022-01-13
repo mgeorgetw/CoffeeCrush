55247ef7863b22a3108a6d506e8931d0f91e1804
Zhaoyuan Huang <mgeorgetw@gmail.com>
2022-01-14 01:34:20 +0800

    Added animation on brew button.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { GetReadyView } from "./views/GetReadyView";
 import { InstructionView } from "./views/InstructionView";
 import brewMethods from "./data/BrewMethods.json";
 import "./App.css";
+import { gsap } from "gsap";
+import { start } from "repl";
 
 function App() {
   const [method, setMethod] = useState("The Ultimate V60");
@@ -16,8 +18,29 @@ function App() {
   );
   const [water, setWater] = useState(chosenMethodDetails!.defaultWaterInMl);
 
+  const animateRef = useRef<HTMLDivElement>(null);
+  const selector = gsap.utils.selector(animateRef);
+  const timelineRef = useRef<GSAPTimeline>();
+
+  const onEnter = ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
+    gsap.to(currentTarget, { opacity: 1, border: "5px solid goldenrod" });
+  };
+
+  const onLeave = ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
+    gsap.to(currentTarget, { opacity: 0.4, border: "none" });
+  };
+
+  useLayoutEffect(() => {
+    timelineRef.current = gsap.timeline().from(selector(".prime-button"), {
+      opacity: 1,
+      border: "5px solid goldenrod",
+      duration: 2,
+      ease: "power2.inOut",
+    });
+  }, [selector]);
+
   return (
-    <div className="App">
+    <div className="App" ref={animateRef}>
       <h1 className="app-title">Coffee Crush</h1>
 
       <GetReadyView
@@ -38,7 +61,12 @@ function App() {
       />
 
       {!isReady && (
-        <button className="prime-button" onClick={() => setIsReady(true)}>
+        <button
+          className="prime-button"
+          onPointerEnter={onEnter}
+          onPointerLeave={onLeave}
+          onClick={() => setIsReady(true)}
+        >
           Brew
         </button>
       )}
