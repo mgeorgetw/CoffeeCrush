508d0154f1d84ea98bf500492f8a41e8d0b8f49b
Zhaoyuan Huang <mgeorgetw@gmail.com>
2022-02-10 16:39:36 +0800

    Refactored for clarity.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { useState, useMemo, useLayoutEffect, useRef, useCallback } from "react";
+import {
+  useState,
+  useMemo,
+  useEffect,
+  useRef,
+  useCallback,
+  PointerEvent,
+} from "react";
 import { GetReadyView } from "./views/GetReadyView";
 import { InstructionView } from "./views/InstructionView";
 import brewMethods from "./data/BrewMethods.json";
@@ -6,59 +13,38 @@ import "./App.css";
 import { gsap } from "gsap";
 
 function App() {
-  const [method, setMethod] = useState<string>("The Ultimate V60");
-  const [isBrewing, setIsBrewing] = useState<boolean>(false);
+  const [method, setMethod] = useState("The Ultimate V60");
   const chosenMethodDetails = useMemo(
     () => brewMethods.find((obj) => obj.name === method),
     [method]
   );
-  const [groundCoffee, setGroundCoffee] = useState<number>(
+  const [groundCoffee, setGroundCoffee] = useState(
     chosenMethodDetails!.defaultCoffeeInGram
   );
-  const [water, setWater] = useState<number>(
-    chosenMethodDetails!.defaultWaterInMl
-  );
+  const [water, setWater] = useState(chosenMethodDetails!.defaultWaterInMl);
+  const [isBrewing, setIsBrewing] = useState(false);
 
-  // Initiate animation elements
-  const animateRef = useRef<HTMLDivElement>(null);
-  const timelineRef = useRef<GSAPTimeline>();
-  const onEnter = ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
-    gsap.to(currentTarget, { opacity: 1, border: "5px solid goldenrod" });
-  };
-  const onLeave = ({ currentTarget }: { currentTarget: HTMLButtonElement }) => {
-    gsap.to(currentTarget, { opacity: 0.4, border: "none" });
-  };
+  const animateRef = useRef<HTMLDivElement>(null!);
+  const selector = gsap.utils.selector(animateRef);
 
   const onScrollToBottom = useCallback(() => {
-    const selector = gsap.utils.selector(animateRef);
+    if (!animateRef.current) throw Error("animateRef is not assigned");
+    reachedAppBottom() && runButtonFadeInAnimation();
 
-    if (animateRef.current) {
+    function reachedAppBottom() {
       const { scrollTop, scrollHeight, clientHeight } = animateRef.current;
-      const reachedBottom: boolean = scrollTop + clientHeight === scrollHeight;
-      reachedBottom &&
-        gsap.to(selector(".prime-button"), {
-          opacity: 1,
-          border: "5px solid goldenrod",
-          duration: 0.5,
-          ease: "power2.inout",
-        });
+      return scrollTop + clientHeight === scrollHeight;
     }
-  }, []);
 
-  useLayoutEffect(() => {
-    const selector = gsap.utils.selector(animateRef);
-
-    const runButtonFadeAnimation = () =>
-      (timelineRef.current = gsap.timeline().from(selector(".prime-button"), {
-        delay: 1,
+    function runButtonFadeInAnimation() {
+      gsap.to(selector(".prime-button"), {
         opacity: 1,
         border: "5px solid goldenrod",
-        duration: 2,
+        duration: 0.5,
         ease: "power2.inout",
-      }));
-
-    !isBrewing && runButtonFadeAnimation();
-  }, [isBrewing]);
+      });
+    }
+  }, [selector]);
 
   return (
     <div className="App" ref={animateRef} onScroll={onScrollToBottom}>
@@ -82,17 +68,55 @@ function App() {
       />
 
       {!isBrewing && (
-        <button
-          className="prime-button"
-          onPointerEnter={onEnter}
-          onPointerLeave={onLeave}
-          onClick={() => setIsBrewing(true)}
-        >
-          Brew
-        </button>
+        <BrewButton isBrewing={isBrewing} setIsBrewing={setIsBrewing} />
       )}
     </div>
   );
 }
+
+const BrewButton = ({
+  isBrewing,
+  setIsBrewing,
+}: {
+  isBrewing: boolean;
+  setIsBrewing: Function;
+}) => {
+  const animatedButtonRef = useRef<HTMLDivElement>(null);
+  const selector = gsap.utils.selector(animatedButtonRef);
+
+  const animationOnEnter = (e: PointerEvent<HTMLButtonElement>) => {
+    gsap.to(e.target, { opacity: 1, border: "5px solid goldenrod" });
+  };
+  const animationOnLeave = (e: PointerEvent<HTMLButtonElement>) => {
+    gsap.to(e.target, { opacity: 0.4, border: "none" });
+  };
+
+  useEffect(() => {
+    !isBrewing && runButtonFadeOutAnimation();
+
+    function runButtonFadeOutAnimation() {
+      gsap.from(selector(".prime-button"), {
+        delay: 1,
+        opacity: 1,
+        border: "5px solid goldenrod",
+        duration: 2,
+        ease: "power2.inout",
+      });
+    }
+  }, [isBrewing, selector]);
+
+  return (
+    <div ref={animatedButtonRef}>
+      <button
+        className="prime-button"
+        onPointerEnter={animationOnEnter}
+        onPointerLeave={animationOnLeave}
+        onClick={() => setIsBrewing(true)}
+      >
+        Brew
+      </button>
+    </div>
+  );
+};
 
 export default App;
