bfab784f9f3aeb2145a1e8f80de7fe616a94e91c
George Huang <eternallogger@gmail.com>
2025-01-14 12:51:52 +0800

    Revert "Preload audio files."
    This reverts commit bda064d09b6447b021e4688067c8b6fdeed3695d.

--- src/views/InstructionView/ProgressDonutChartButton/index.tsx
+++ src/views/InstructionView/ProgressDonutChartButton/index.tsx
@@ -19,37 +19,19 @@ export const ProgressDonutChartButton = ({
   isRunning: boolean;
   setIsRunning: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-  const audioRefs = useRef({
-    buttonClickedAudio: new Audio(),
-    bellAudio: new Audio(),
-  });
+  const audioRef = useRef<HTMLAudioElement>(new Audio());
 
-  const audioSources = {
-    buttonClickedAudio: "/CoffeeCrush/assets/start",
-    bellAudio: "/CoffeeCrush/assets/bell",
-  };
+  const buttonClickedAudio = "/CoffeeCrush/assets/start.mp3";
+  const bellAudio = "/CoffeeCrush/assets/bell.mp3";
 
-  function playAudio(audioRef: HTMLAudioElement) {
-    audioRef.play().catch((err) => console.error("Audio play failed:", err));
+  function playAudio(src: string) {
+    const audio = audioRef.current;
+    audio.src = src;
+    audio.play().catch((err) => console.error("Audio play failed:", err));
   }
 
   useEffect(() => {
-    // Preload audio files
-    Object.entries(audioRefs.current).forEach(([key, audioRef]) => {
-      const baseSrc = audioSources[key as keyof typeof audioSources];
-      audioRef.innerHTML = `
-        <source src="${baseSrc}.mp3" type="audio/mpeg">
-        <source src="${baseSrc}.ogg" type="audio/ogg">
-      `;
-      audioRef.preload = "auto";
-      audioRef.load();
-    });
-  }, []);
-
-  useEffect(() => {
-    const { bellAudio } = audioRefs.current;
     const isStepsChanging: boolean = stepsLeft > 0 && untilNextStep === 1;
-
     if (isStepsChanging) {
       playAudio(bellAudio);
     }
@@ -59,20 +41,10 @@ export const ProgressDonutChartButton = ({
     <div
       id="donutProgressChart"
       onClick={() => {
-        const { buttonClickedAudio } = audioRefs.current;
         setIsRunning(!isRunning);
         playAudio(buttonClickedAudio);
       }}
     >
-      {/* Audio elements are preloaded */}
-      {Object.keys(audioSources).map((key) => (
-        <audio
-          key={key}
-          ref={(el) => {
-            if (el) audioRefs.current[key as keyof typeof audioRefs.current] = el;
-          }}
-        ></audio>
-      ))}
       <DonutProgressContainer
         time={time}
         data={steps}
@@ -83,4 +55,3 @@ export const ProgressDonutChartButton = ({
     </div>
   );
 };
-
